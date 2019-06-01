7efbe53b0c47919e4c0ca7d466aa015f88c5b1a4
一条菜狗 <bypl@vip.qq.com>
2019-06-01 22:19:12 +0800

    轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1609/88/2e8d6b75aec65502.jpg_750x200_73f19816.jpg'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cce3755aba84e080c2479364b1284152.jpg_750x200_6084e96d.jpg'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/a6c35104d1717428ed36051594d0f96e.jpg_750x200_06f3b9d8.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #ccc
+    .swiper-img
+      width: 100%
+</style>
