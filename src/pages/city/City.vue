c1df14b8dd4043e8b5199b1ead2b72cda2bd0171
一条菜狗 <bypl@vip.qq.com>
2019-06-12 23:53:56 +0800

    finish list css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    city
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
-import cityHeader from './components/Header'
-import citySearch from './components/Search'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 
 export default {
   name: 'City',
   components: {
-    cityHeader,
-    citySearch
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
