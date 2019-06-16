6c28b7754ce4e51ff92ccb9a22b4c38ab15ccff7
一条菜狗 <bypl@vip.qq.com>
2019-06-16 23:52:25 +0800

    列表联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) in cities" :key="key">{{ key }}</li>
+    <li
+      class="item"
+      v-for="item in letters"
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >{{ item }}</li>
   </ul>
 </template>
 
@@ -9,6 +18,36 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index <= this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -24,7 +24,8 @@
       <div
         class="area"
         v-for="(item, key) in cities"
-        :key="item.id"
+        :key="key"
+        :ref="key"
       >
         <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
@@ -45,10 +46,17 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      const element = this.$refs[this.letter][0]
+      this.scroll.scrollToElement(element)
+    }
   }
 }
 </script>
